8ef33ebaebbadc52c27be883f76a3c4e080a0e05
AndresDiazPinto <andresvis565@gmail.com>
2022-09-27 20:28:52 -0500

    Ajustes
    Se vinculan videos

--- src/views/Tema3.vue
+++ src/views/Tema3.vue
@@ -18,7 +18,7 @@
 
     figure.mb-5
       .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/Ni_fGjSGgIo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
     
     TraduccionAudio.color-acento-contenido.pst_9.mb-5(:dialogo="dialogo2")(data-aos="flip-up") 
 
